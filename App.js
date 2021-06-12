59c9470564e228d020c6cc96e8c196601c407308
Jeronimo Mendivil <jeronimomendivil@gmail.com>
2021-06-12 19:21:34 -0300

    comienzo de autenticacion y dataStorage

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
+import React, {useState, useEffect} from "react"
+import { StatusBar} from "react-native"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import { NavigationContainer } from "@react-navigation/native"
+
+import AsyncStorage from "./utils/asyncStorage"
 
 import Home from "./pages/home"
 import About from "./pages/about"
@@ -12,18 +15,49 @@ import Juego from "./pages/juego"
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+
+  const [authenticated, setAuthenticated] = useState(false)
+
+  const checkUser = async () => {
+    const user = await AsyncStorage.getData('@userData')
+    console.log(user)
+    if (user) {
+      setAuthenticated(true)
+    }
+  }
+
+  useEffect( () =>{
+    checkUser()
+  },[])
+
+  const applyAuthentication = (user) =>{
+    console.log('Data a persistir: ', user)
+    AsyncStorage.storeData('@userData', user)
+
+  }
+
+
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName={'Home'}>
-        <Drawer.Screen name={'Home'} component={Home} />
-        <Drawer.Screen name={'Niveles'} component={Niveles} />
-        <Drawer.Screen name={'Juego'} component={Juego} />
-        <Drawer.Screen name={'Ranking'} component={Ranking} />
-        <Drawer.Screen name={'About'} component={About} />
-
-
-
-      </Drawer.Navigator>
+      <StatusBar style="auto" />
+      
+        <Drawer.Navigator>
+          {(authenticated) ?
+          <>
+            <Drawer.Screen name={'Niveles'} component={Niveles} />
+            <Drawer.Screen name={'Juego'} component={Juego} />
+            <Drawer.Screen name={'Ranking'} component={Ranking} />
+            <Drawer.Screen name={'About'} component={About} />
+          </>
+          :
+          <>
+            <Drawer.Screen name={'Home'} component={Home} />
+            <Drawer.Screen name={'About'} component={About} />
+          </>
+          }
+         </Drawer.Navigator>
+      
     </NavigationContainer>
   );
 }
